7e5f44f343b48b838133fa0fc507ff8bb0100106
Pranay Singhal <pranay_singhal@hotmail.com>
2023-04-10 16:44:32 -0400

    updated Web server's index page

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # terraform-aws-autoscaling-demo
+
+# Generating CPU stress on one of the EC2 instances
+sudo amazon-linux-extras install epel -y  
+sudo yum install -y stress
+
+stress --cpu 4 --timeout 300
--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ yum update -y
 yum install -y httpd
 yum install -y wget
 cd /var/www/html
-echo -n "<html><body><h1>Hello World, I am the AWS auto-scaling demo.</h1></body></html>" > index.html
+echo -n "<html><body><h1>Hello World, I am server $(hostname -f) in the AWS auto-scaling demo.</h1></body></html>" > index.html
 service httpd start
